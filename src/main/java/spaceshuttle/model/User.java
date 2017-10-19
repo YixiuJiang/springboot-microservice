b495b4bf266dbcdf211550efbc13f99222b90cc4
Uruz7 <arockhe@gmail.com>
2017-10-20 00:57:36 +1100

    commit for 1st assignment on 18 Nov Rock

--- src/main/java/spaceshuttle/model/User.java
+++ src/main/java/spaceshuttle/model/User.java
@@ -6,12 +6,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class  User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
