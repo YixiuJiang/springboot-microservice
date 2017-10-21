2104d0300b64d74b2ddde19712cfa9a5f6f5f9fb
Simon Qi <smilexx2@gmail.com>
2017-10-21 19:07:03 +1100

    Add AllResponse class

---
+++ src/main/java/spaceshuttle/model/AllResponse.java
@@ -0,0 +1,31 @@
+package spaceshuttle.model;
+
+public class AllResponse {
+    private boolean isSuccess;
+    private String errorCode;
+    private String errorMassage;
+
+    public boolean isSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(boolean success) {
+        isSuccess = success;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMassage() {
+        return errorMassage;
+    }
+
+    public void setErrorMassage(String errorMassage) {
+        this.errorMassage = errorMassage;
+    }
+}
